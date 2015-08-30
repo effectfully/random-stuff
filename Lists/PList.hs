707b3fdb022af1a6b60b9b3dab39ca75cee39348
effectfully <effectfully@gmail.com>
2015-08-30 13:59:45 +0300

    updated PList

--- Lists/PList.hs
+++ Lists/PList.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE Rank2Types #-}
 
 import Data.Maybe
+import Data.List
 
 newtype PList a = PList { runPList :: forall r. (PList a -> a -> r -> r) -> r -> r }
 
@@ -19,15 +20,18 @@ pfoldr f z xs = runPList xs (\_ -> f) z
 pfuse :: PList a -> PList a
 pfuse = pfoldr pcons pnil
 
+pappend :: PList a -> PList a -> PList a
+pappend xs ys = PList $ \f -> runPList xs f . runPList ys f
+
 pmap :: (a -> b) -> PList a -> PList b
 pmap h xs = PList $ \f -> runPList xs (\xs' -> f (pmap h xs') . h)
 
 pzipWith :: (a -> b -> c) -> PList a -> PList b -> PList c
-pzipWith f xs ys = go f (pfuse xs) (pfuse ys) where
-	go f xs ys = fromMaybe pnil $ do
+pzipWith f xs ys = go (pfuse xs) (pfuse ys) where
+	go xs ys = fromMaybe pnil $ do
 		(x, xs') <- psplit xs
 		(y, ys') <- psplit ys
-		return $ f x y `pcons` go f xs' ys'
+		return $ f x y `pcons` go xs' ys'
 
 pzip :: PList a -> PList b -> PList (a, b)
 pzip = pzipWith (,)
@@ -45,3 +49,6 @@ main = do
 -- [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9)]	
 	print $ ptoList $ pzip (pfromList [1..9 ]) (pfromList [1..10])
 -- [(1,1),(2,2),(3,3),(4,4),(5,5),(6,6),(7,7),(8,8),(9,9)]
+	print $ length . ptoList $ foldl' pappend pnil . map (pfromList . (:[])) $ [1..1000000]
+-- 1000000
+
